a2ac8e04130e0084f0ddd7fc8b19a0c2ab5567de
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2025-10-22 12:09:09 +0000

    simplify R scripts

--- scripts/preview.R
+++ scripts/preview.R
@@ -1,13 +1,1 @@
-
-# Arguments for the preview 
-args = commandArgs(trailingOnly=TRUE)
-
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("One argument must be supplied.", call.=FALSE)
-}
-
-# Preview website
-if (args[1] == "yes") {
-    servr::httw(paste0(getwd(), '/_site'))
-}
+servr::httw(paste0(getwd(), '/_site'))
--- scripts/render.R
+++ scripts/render.R
@@ -3,15 +3,5 @@ if (! "babelquarto" %in% installed.packages() ) {
     install.packages('babelquarto', repos = c('https://ropensci.r-universe.dev', 'https://cloud.r-project.org'))
 }
 
-# Arguments for the preview 
-args = commandArgs(trailingOnly=TRUE)
-
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("One argument must be supplied.", call.=FALSE)
-}
-
-if (args[1] == "yes") {
-    # Render site
-    babelquarto::render_website(getwd())
-}
+# Render site
+babelquarto::render_website(getwd())
