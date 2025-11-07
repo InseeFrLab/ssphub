fc7b7674a1dd67a68362ce299291b8d50155f21e
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2025-11-07 13:17:39 +0000

    update to match call from shell

--- scripts/template.py
+++ scripts/template.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import yaml
-
+import sys
 
 def find_index_qmd_files(root_folder='ssphub/project'):
     index_qmd_files = []
@@ -18,12 +18,12 @@ def extract_categories(file_path='ssphub/project/2019_gdp_tracker/index.qmd'):
     Extract categories of the YAML part of index.qmd
 
     Args:
-        
+
     Returns:
         list
 
     Example:
-        >>> 
+        >>>
     """
     with open(file_path, mode='r') as file:
         qmd_content=file.read()
@@ -33,7 +33,7 @@ def extract_categories(file_path='ssphub/project/2019_gdp_tracker/index.qmd'):
     yaml_header = yaml.safe_load(yaml_header)
 
     return yaml_header.get('categories', '')
-    
+
 
 def conc_yaml_categories(root_folder='ssphub/project'):
     # Parse the YAML header1
@@ -50,11 +50,11 @@ def conc_yaml_categories(root_folder='ssphub/project'):
 
 # conc_yaml_categories()
 
-def add_categories_to_yaml(qmd_output_file, template_path='ssphub/scripts/project_template.qmd', root_folder='ssphub/project'):
+def add_categories_to_yaml(qmd_output_file, template_path='project_template.qmd', root_folder='../project'):
     with open(template_path, mode='r') as file:
         qmd_content=file.read()
     parts = qmd_content.split('---', 2)
-    
+
     yaml_header = parts[1]
     md_content = parts[2]
 
@@ -66,7 +66,7 @@ def add_categories_to_yaml(qmd_output_file, template_path='ssphub/scripts/projec
 
     # Convert the cleaned YAML back to a string
     yaml_header_str = yaml.dump(yaml_header, sort_keys=False,  allow_unicode=True, width=4096)
-    
+
     # Combine the cleaned YAML header and HTML content
     processed_qmd_content = f"---\n{yaml_header_str}---\n{md_content}"
 
@@ -76,14 +76,23 @@ def add_categories_to_yaml(qmd_output_file, template_path='ssphub/scripts/projec
 
 # add_categories_to_yaml('test.qmd')
 
-def create_folder_and_file(folder_name, template_path='ssphub/scripts/project_template.qmd'):
+def create_folder_and_file(folder_name, template_path='project_template.qmd'):
     # Create the folder in the 'project' directory
-    os.makedirs(os.path.join("ssphub/project", folder_name), exist_ok=True)
+    os.makedirs(os.path.join("../project", folder_name), exist_ok=True)
 
     # Define the path for the new file
-    new_file_path = os.path.join("ssphub/project", folder_name, "index.qmd")
+    new_file_path = os.path.join("../project", folder_name, "index.qmd")
 
     # Copy the template to the new file
-    add_categories_to_yaml(new_file_path, template_path=template_path, root_folder="ssphub/project")
+    add_categories_to_yaml(new_file_path, template_path=template_path, root_folder="../project")
 
-create_folder_and_file("2022_JOCAS")
+
+if __name__ == '__main__':
+    foldername = sys.argv[1]
+    if len(sys.argv) > 2:
+        template_path = sys.argv[2]
+        create_folder_and_file(foldername, template_path)
+    else:
+        create_folder_and_file(foldername)
+
+
