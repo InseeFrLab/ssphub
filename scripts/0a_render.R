d1238c9c5719966d37c40858ed335b4fce39e682
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-18 09:46:30 +0000

    update names

--- scripts/0a_render.R
+++
@@ -1,7 +0,0 @@
-# Installing babelquarto if not present
-if (! "babelquarto" %in% installed.packages() ) {
-    install.packages('babelquarto', repos = c('https://ropensci.r-universe.dev', 'https://cloud.r-project.org'))
-}
-
-# Render site
-babelquarto::render_website(getwd())
--- scripts/2_translate.R
+++ scripts/2_translate.R
@@ -1,23 +1,23 @@
 # Installing babelquarto if not present
-if (! "pak" %in% installed.packages() ) {
-    install.packages("pak")
-    pak::pak("ropensci-review-tools/babeldown")
-}
+# if (! "pak" %in% installed.packages() ) {
+#     install.packages("pak")
+#     pak::pak("ropensci-review-tools/babeldown")
+# }
 
 # Arguments for the preview
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 
 # test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("One argument must be supplied.", call.=FALSE)
+if (length(args) == 0) {
+  stop("One argument must be supplied.", call. = FALSE)
 }
 
-if (! "DEEPL_API_KEY" %in% names(Sys.getenv())){
-    stop("DEEPL API KEY not set", call.=FALSE)
+if (!"DEEPL_API_KEY" %in% names(Sys.getenv())) {
+  stop("DEEPL API KEY not set", call. = FALSE)
 }
 
-input_path = paste0("project/", args[1], "/index.qmd")  # sth like directory/sub
-out_path = paste0("project/",args[1], "/index.en.qmd")
+input_path = paste0("project/", args[1], "/index.qmd") # sth like directory/sub
+out_path = paste0("project/", args[1], "/index.en.qmd")
 
 babeldown::deepl_translate(
   path = input_path,
@@ -36,7 +36,11 @@ replace_in_qmd <- function(input_file) {
   content <- gsub("\\s{5,}", "", content)
 
   # Replace too many "-"s with as many necessary to fill the first line
-  content <- gsub("-{77,}", "-------------------------------------------------------------------------", content)
+  content <- gsub(
+    "-{77,}",
+    "-------------------------------------------------------------------------",
+    content
+  )
 
   # Replace "|  | ----"
   content <- gsub("\\|\\s{2}\\|\\s---", "|---|---", content)
@@ -45,18 +49,21 @@ replace_in_qmd <- function(input_file) {
   content <- gsub("\\\\", "", content)
 
   # Replace "description: |-" or "description: | -" with "description: |"
-  content <- gsub("description:\\s\\|-|description:\\s\\|\\s-", "description: |", content)
+  content <- gsub(
+    "description:\\s\\|-|description:\\s\\|\\s-",
+    "description: |",
+    content
+  )
 
   # Write the modified content to the output file
   writeLines(content, input_file)
-
 }
 
 replace_in_qmd(out_path)
 
 # NO WORK - TO DO LATER
 # # Glossary
-# filename <- "glossary-fr-en.csv"
+# filename <- "2_glossary-fr-en.csv"
 
 # # create (or update) glossary
 # babeldown::deepl_upsert_glossary(
