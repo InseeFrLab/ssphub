62f3a46a1631eb8613d24c5432ca50b9349ee341
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-25 16:58:40 +0000

    update template extension to remove from rendering

--- scripts/init_infolettre.sh
+++ scripts/init_infolettre.sh
@@ -8,6 +8,6 @@ git switch -c $INFOLETTRE_BRANCH
 git push --set-upstream origin $INFOLETTRE_BRANCH 
 
 cp -r infolettre/infolettre_XX infolettre/$INFOLETTRE_BRANCH
-mv infolettre/$INFOLETTRE_BRANCH/template.qmd infolettre/$INFOLETTRE_BRANCH/index.qmd 
+mv infolettre/$INFOLETTRE_BRANCH/template.txt infolettre/$INFOLETTRE_BRANCH/index.qmd 
 
 git add infolettre/$INFOLETTRE_BRANCH/index.qmd && git commit -m "initializing infolettre $INFOLETTRE_NB"
