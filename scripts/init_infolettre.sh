3927544a82218b8b23b18b4cb08b5740407fe550
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-25 16:40:30 +0000

    script to start infolettre

---
+++ scripts/init_infolettre.sh
@@ -0,0 +1,11 @@
+
+INFOLETTRE_NB=$1
+INFOLETTRE_BRANCH="infolettre_$INFOLETTRE_NB"
+
+git switch main
+git pull origin main
+git switch -c $INFOLETTRE_BRANCH 
+git push --set-upstream origin $INFOLETTRE_BRANCH 
+
+cp -r infolettre/infolettre_XX infolettre/$INFOLETTRE_BRANCH
+mv infolettre/$INFOLETTRE_BRANCH/template.qmd infolettre/$INFOLETTRE_BRANCH/index.qmd 
